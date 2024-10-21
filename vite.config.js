b10f9999589c6f454bf7a435695326069a70efcd
Teju1626 <yeachuritejaswini@gmail.com>
2024-10-21 20:09:20 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/SOS1/', // Make sure this matches your repository name
+});
